a38370b5a08ed466580b7ddc2b00ec04e863e8bc
Yijie <yijie02281@hotmail.com>
2023-12-07 17:13:20 +0800

    Modified a few files according to the course

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -24,6 +24,8 @@ router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializer import MenuSerializer, BookingSerializer
 
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
+    authentication_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -12,5 +14,6 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     pass
 
 class BookingViewSet(viewsets.ModelViewSet):
+    authentication_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
